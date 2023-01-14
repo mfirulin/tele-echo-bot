bcd21cf455cd502ff1fd0663279402bd3f633c21
Mikhail Firulin <mfirulin@google.com>
2023-01-14 17:36:59 +0300

    Command handling added

--- src/main/java/mfirulin/echobot/Bot.java
+++ src/main/java/mfirulin/echobot/Bot.java
@@ -3,14 +3,17 @@ package mfirulin.echobot;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
+    private boolean screaming = false;
+
     @Override
     public String getBotUsername() {
-        return "mfirulin_echo_bot";
+        return "mfirulin_echo";
     }
 
     @Override
@@ -23,16 +26,26 @@ public class Bot extends TelegramLongPollingBot {
         var msg = update.getMessage();
         var msgId = msg.getMessageId();
         var userId = msg.getFrom().getId();
+
+        if (msg.isCommand()) {
+            handleCommand(msg.getText());
+            return; // We don't want to echo commands, so we exit
+        }
     
         try {
             // sendText(userId, msg.getText());
-            copyMessage(userId, msgId);
+            // copyMessage(userId, msgId);
+            if (screaming) {
+                scream(userId, update.getMessage()); // Call a custom method
+            } else {
+                copyMessage(userId, msg.getMessageId()); // Else proceed normally
+            }
         } catch (TelegramApiException e) {
             System.err.println(e);
         }
     }
 
-    public void sendText(Long who, String what) throws TelegramApiException {
+    private void sendText(Long who, String what) throws TelegramApiException {
         var msg = SendMessage.builder()
             .chatId(who.toString()) // Who we are sending a message to
             .text(what) // Message content
@@ -41,7 +54,7 @@ public class Bot extends TelegramLongPollingBot {
         execute(msg);
     }
 
-    public void copyMessage(Long who, Integer msgId) throws TelegramApiException {
+    private void copyMessage(Long who, Integer msgId) throws TelegramApiException {
         var msg = CopyMessage.builder()
             .fromChatId(who.toString()) // We copy from the user
             .chatId(who.toString()) // And send it back to him
@@ -49,6 +62,22 @@ public class Bot extends TelegramLongPollingBot {
             .build();
 
         execute(msg);
+    }
+
+    private void handleCommand(String cmd) {
+        screaming = switch(cmd) {
+            case "/scream" -> true; // If the command was /scream, we switch gears
+            case "/whisper" -> false; // Otherwise, we return to normal
+            default -> false;
+        };
+    }
+
+    private void scream(Long id, Message msg) throws TelegramApiException {
+        if (msg.hasText()) {
+            sendText(id, msg.getText().toUpperCase());
+        } else {
+            copyMessage(id, msg.getMessageId());  // We can't really scream a sticker
+        }
     }
 
 }
